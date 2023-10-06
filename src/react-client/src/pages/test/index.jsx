c8be0ce2781ac8aacba1bae44e4a712c1c227267
Jehee Cheon <jeheecheon@gmail.com>
2023-10-06 17:43:24 +0900

    modal 패키지 설치

--- src/react-client/src/pages/test/index.jsx
+++ src/react-client/src/pages/test/index.jsx
@@ -1,12 +1,27 @@
 import { Button } from "react-bootstrap";
 import { API_BASE_URL } from "../../configs/api-endpoints";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
+
+import Modal from 'react-modal';
 
 export default function Test() {
     const Navigate = useNavigate();
     const { user, setUser } = useContext(UserContext);
+    const [modalIsOpen, setIsOpen] = useState(true);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
 
     function TestCall() {
         fetch(API_BASE_URL + "/api/test/test", {
@@ -38,6 +53,17 @@ export default function Test() {
     return (
         <>
             <Button onClick={TestCall}>Click me</Button>
+
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                // style={customStyles}
+                contentLabel="Example Modal"
+            >
+                Hello Modal!
+            </Modal>
+
             {/* <Button onClick={GoogleLogin}>Google login</Button> */}
         </>
     );
