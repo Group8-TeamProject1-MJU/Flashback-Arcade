230a437a072963f5d1bfc7e1c0016484ec38cb24
Jang <jyc4833@gmail.com>
2023-10-17 19:21:27 +0900

    점수 추가 수정

--- src/Application/Ranking/Rankers.cs
+++ src/Application/Ranking/Rankers.cs
@@ -41,7 +41,11 @@ public class Rankers {
     // _scores에 새로운 랭커 삽입 및 꼴지 제거.
     // 삽입 후 정렬된 상태가 유지되어야 함
     public bool TryAdd(ScoreHistory scoreHistoryToAdd) {
-        LinkedListNode<ScoreHistory> currentNode = scores.First;
+        System.Console.WriteLine("asdasdasdasdasd");
+        foreach(var score in scores){
+            Console.WriteLine($"{score.Score} {score.UserId}");
+        }
+        var currentNode = scores.First;
 
         // 전달된 점수가 다른 종류의 게임 점수이면 리턴
         if (!CheckSameGame(scoreHistoryToAdd))
@@ -57,19 +61,16 @@ public class Rankers {
         }
         // scores 변수에 정렬된 상태를 유지하면서 새로운 노드 삽입
         while(currentNode != null){
-            if(Compare(scoreHistoryToAdd.Score, currentNode.Value.Score)){
-                scores.AddBefore(currentNode,scoreHistoryToAdd);
-                if(scores.Count > 10){
-                    scores.RemoveLast();
+            if(scores.Count > 10){
+                scores.RemoveLast();
+            }else{
+                if(Compare(scoreHistoryToAdd.Score, currentNode.Value.Score) || scoreHistoryToAdd.Score == currentNode.Value.Score){
+                    scores.AddBefore(currentNode,scoreHistoryToAdd);
+                }else{
+                    scores.AddLast(scoreHistoryToAdd);
                 }
-                return true;
             }
             currentNode = currentNode.Next;
-        }
-
-        if(scores.Count < 10){
-            scores.AddLast(scoreHistoryToAdd);
-            return true;
         }
 
         // TODO: _scores변수에 정렬된 상태를 유지하면서 새로운 노드를 삽입!
@@ -77,8 +78,7 @@ public class Rankers {
         // TODO: 같은 유저가 중복으로 존재해서는 안됨
         // TODO: _descending이 true이면 내림차순으로, false이면 오름차순으로 정렬되어야 함
         // TODO: 그래서 가능하다면 이미 만들어둔 Compare 메소드를 쓰면 좋음
-
-        return false;
+        return true;
         // return true;
     }
 }
