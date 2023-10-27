597c8bc59aada2b5ae7c8d963fb92172e8f8c3b0
Jehee Cheon <jeheecheon@gmail.com>
2023-10-27 17:51:19 +0900

    Quick Speed Master 게임 DB에 업데이트 및 migration 생성

---
+++ src/Infrastructure/Migrations/MainDb/20231027084914_Quick Speed Master 게임 추가.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations.MainDb
+{
+    /// <inheritdoc />
+    public partial class QuickSpeedMaster게임추가 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Game",
+                columns: new[] { "Id", "Descending", "Title" },
+                values: new object[] { 8, false, "Qucik Speed Master" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Game",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
