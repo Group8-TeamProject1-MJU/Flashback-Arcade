87d355f55290b2509bbc9f54039db4aecb19e906
Jehee Cheon <jeheecheon@gmail.com>
2023-10-29 15:07:33 +0900

    페이지네이션 버튼 UI 생성

--- src/react-client/src/components/GameRankingBoard.jsx
+++ src/react-client/src/components/GameRankingBoard.jsx
@@ -3,9 +3,31 @@ import AppRoutes from "../utils/AppRoutes";
 import { useContext } from "react";
 import { GameRankersContext } from "../contexts/GameRankersContext";
 
-
 export function GameRankingBoard() {
+    let rankersCnt = 0;
     const { rankers, game } = useContext(GameRankersContext);
+
+    function PrintRest() {
+        let elements = [];
+
+        for (let i = rankersCnt; i < 10; ++i) {
+            elements.push(
+                <Row className="justify-content-between" key={i}>
+                    <Col className="board-col">
+                        {i + 1}
+                    </Col>
+                    <Col className="board-col">
+                        XXX
+                    </Col>
+                    <Col className="board-col">
+                        XXX
+                    </Col>
+                </Row>
+
+            )
+        }
+        return elements;
+    }
 
     return (
         <>
@@ -30,6 +52,7 @@ export function GameRankingBoard() {
                             </Row>
                             {
                                 rankers && rankers.map((ranker, idx) => {
+                                    ++rankersCnt;
                                     return (
                                         <Row className="justify-content-between" key={ranker.Id}>
                                             <Col className="board-col">
@@ -45,6 +68,18 @@ export function GameRankingBoard() {
                                     )
                                 })
                             }
+                            {PrintRest()}
+                            <Row className="justify-content-center">
+                                <Col className="pagination-button">
+                                    &lt;
+                                </Col>
+                                <Col className="pagination-button">
+                                    &gt;
+                                </Col>
+                            </Row>
+                        </Container>
+
+                        <Container>
                         </Container>
                     </div>
                 </div>
