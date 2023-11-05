1a1acc8be681fa1a96eb2d623d83b7e6ea1791da
Jehee Cheon <jeheecheon@gmail.com>
2023-11-05 14:06:35 +0900

    채팅창 동접자 수 기능

--- src/Server/Hubs/ChatHub.cs
+++ src/Server/Hubs/ChatHub.cs
@@ -3,6 +3,21 @@ using Microsoft.AspNetCore.SignalR;
 namespace Server.Hubs;
 
 public class ChatHub : Hub {
+    // 정적 변수로 연결된 클라이언트 수 추적
+    private static int connectedClients = 0;
+
+    public async override Task OnConnectedAsync() {
+        ++connectedClients;
+        await Clients.All.SendAsync("UpdateConnectedClientsCount", connectedClients);
+        await base.OnConnectedAsync();
+    }
+
+    public async override Task OnDisconnectedAsync(Exception exception) {
+        --connectedClients;
+        await Clients.All.SendAsync("UpdateConnectedClientsCount", connectedClients);
+        await base.OnDisconnectedAsync(exception);
+    }
+
     public async Task SendMessage(string user, string message) {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
