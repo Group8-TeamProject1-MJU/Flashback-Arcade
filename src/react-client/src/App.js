c11fbf6b8ec6cc05cf6bcb6a4e2e76db49b08468
jiwon0122 <wldnjssem6@naver.com>
2023-11-14 19:18:06 +0900

    시간 경과 알림
    시간 경과 알림

--- src/react-client/src/App.js
+++ src/react-client/src/App.js
@@ -5,21 +5,40 @@ import { ToastContainer, toast } from 'react-toastify';
 import MusicButton from './components/MusicButton';
 import { Suspense, useEffect, useState } from 'react';
 import CustomPacmanLoader from './components/PacmanLoader';
+import axios from 'axios';
+
 
 function App() {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setPlaytime(0);
     if (process.env.NODE_ENV !== 'development') {
       setLoading(true);
       setTimeout(() => {
         setLoading(false);
       }, 3000);
     }
+    const intervalId = setInterval(() => {
+     
+      setPlaytime((playtime)=>playtime+1);
+      toast(`${playtime+1}시간이 경과되었습니다.`)
+    },1000); // 1시간
+
+    // 컴포넌트가 언마운트될 때 interval 정리
+    return () => clearInterval(intervalId);
+  
 
   }, []);
 
+  const [playtime, setPlaytime] = useState(null);
+
+ 
+ 
   return (
     <UserProvider>
+      <div>
+      <p color="white">{playtime !== null ? `Total Playtime: ${playtime}` : 'Loading playtime...'}</p>
+    </div>
       {loading ? (
         <div className="loading-animation">
           <CustomPacmanLoader className="" />
@@ -42,6 +61,8 @@ function App() {
       )}
     </UserProvider>
   );
+
+  
 }
 
 export default App;
