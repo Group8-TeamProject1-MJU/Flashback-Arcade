63e58af3bc9189fae738787c0dc6cc1028cafa5c
Jehee Cheon <jeheecheon@gmail.com>
2023-11-17 02:11:56 +0900

    원본언어 감지 기능 추가

--- src/Server/Controllers/ChatController.cs
+++ src/Server/Controllers/ChatController.cs
@@ -18,36 +18,48 @@ namespace Server.Controllers;
 [Route("api/[controller]")]
 public class ChatController : ControllerBase {
     private readonly ILogger _logger;
-    private readonly HttpClient _httpClient;
+    private readonly IServiceProvider _serviceProvider;
+
     private readonly IConfiguration _configuration;
 
     public ChatController(
         ILogger<TestController> logger,
-        HttpClient httpClient,
-        IConfiguration configuration
+        IConfiguration configuration,
+        IServiceProvider serviceProvider
     ) {
         _logger = logger;
-        _httpClient = httpClient;
         _configuration = configuration;
+        _serviceProvider = serviceProvider;
     }
 
     [HttpGet("translate")]
-    public async Task<IActionResult> Translate(string textToTransltate, string translateFrom, string translateTo) {
+    public async Task<IActionResult> Translate(string textToTransltate, string translateTo) {
+        // 메시지의 언어 코드를 구함
+        string translateFrom = await DetectLangs(textToTransltate);
+        if (string.IsNullOrWhiteSpace(translateFrom)) {
+            _logger.LogInformation("translateFrom is null");
+            return BadRequest();
+        }
+
+        // translateTo 와 translateFrom이 같다면
+        if (translateFrom.CompareTo(translateTo) == 0) 
+            return Ok(textToTransltate);
+        
+        using HttpClient httpClient = _serviceProvider.GetRequiredService<HttpClient>();
+
+        // 네이버 파파고 api 주소 및 헤더, 바디 세팅
         string url = "https://openapi.naver.com/v1/papago/n2mt";
-
-        _httpClient.DefaultRequestHeaders.Add("X-Naver-Client-Id", _configuration["Authentication:Naver:ClientId"]);
-        _httpClient.DefaultRequestHeaders.Add("X-Naver-Client-Secret", _configuration["Authentication:Naver:ClientSecret"]);
-
+        httpClient.DefaultRequestHeaders.Add("X-Naver-Client-Id", _configuration["Authentication:Naver:ClientId"]);
+        httpClient.DefaultRequestHeaders.Add("X-Naver-Client-Secret", _configuration["Authentication:Naver:ClientSecret"]);
         var content = new StringContent($"source={translateFrom}&target={translateTo}&text={textToTransltate}", Encoding.UTF8, "application/x-www-form-urlencoded");
 
-        var response = await _httpClient.PostAsync(url, content);
+        // api 요청
+        var response = await httpClient.PostAsync(url, content);
 
         if (response.IsSuccessStatusCode) {
             string result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
 
             using (JsonDocument doc = JsonDocument.Parse(result)) {
-                // root를 가져오고 필요한 경로로 이동
                 JsonElement root = doc.RootElement;
                 JsonElement messageElement = root.GetProperty("message");
                 JsonElement resultElement = messageElement.GetProperty("result");
@@ -68,6 +80,45 @@ public class ChatController : ControllerBase {
         else {
             _logger.LogInformation($"Error: {response.StatusCode} - {response.ReasonPhrase}");
             return BadRequest();
+        }
+    }
+
+    [NonAction]
+    public async Task<string> DetectLangs(string text) {
+        using HttpClient httpClient = _serviceProvider.GetRequiredService<HttpClient>();
+
+        // 네이버 파파고 api 주소 및 헤더, 바디 세팅\
+        string url = "https://openapi.naver.com/v1/papago/detectLangs";
+        httpClient.DefaultRequestHeaders.Add("X-Naver-Client-Id", _configuration["Authentication:Naver:ClientId"]);
+        httpClient.DefaultRequestHeaders.Add("X-Naver-Client-Secret", _configuration["Authentication:Naver:ClientSecret"]);
+        var content = new StringContent($"query={text}", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+        // api 요청
+        var response = await httpClient.PostAsync(url, content);
+
+        // OK 일 때
+        if (response.IsSuccessStatusCode) {
+            string result = await response.Content.ReadAsStringAsync();
+
+            using (JsonDocument doc = JsonDocument.Parse(result)) {
+                JsonElement root = doc.RootElement;
+                JsonElement langCodeElement = root.GetProperty("langCode");
+
+                // 언어코드 추출
+                string langCode = langCodeElement.GetString()!;
+
+                if (string.IsNullOrWhiteSpace(langCode)) {
+                    _logger.LogInformation("Error detecting language.");
+                    return string.Empty;
+                }
+
+                return langCode;
+            }
+        }
+        // 에러..
+        else {
+            _logger.LogInformation($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+            return string.Empty;
         }
     }
 }
