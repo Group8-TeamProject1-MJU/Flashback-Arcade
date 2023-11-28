6ef2e6688a43b7e0779b3714cebae5aeb6c0f2a2
Jehee Cheon <jeheecheon@gmail.com>
2023-11-28 18:30:45 +0900

    병합 충돌 및 버그 수정

--- src/Application/Chat/ChatMessageWithTranslation.cs
+++ src/Application/Chat/ChatMessageWithTranslation.cs
@@ -6,9 +6,16 @@ public class ChatMessageWithTranslation : ChatMessage {
     public string translation { get; set; } = string.Empty;
     public bool showTranslatedText = false;
 
+    public ChatMessageWithTranslation() { }
     public ChatMessageWithTranslation(ChatMessage chatMessage) {
         this.message = chatMessage.message;
         this.sentTime = chatMessage.sentTime;
         this.userName = chatMessage.userName;
+    }
+
+    public ChatMessageWithTranslation(string msg, DateTime time, string name) {
+        this.message = msg;
+        this.sentTime = time;
+        this.userName = name;
     }
 }
